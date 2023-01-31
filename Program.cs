2e8773e0fb5f72f94d43006c73feedd00df3cdf3
ilyaBobrov <rog89@bk.ru>
2023-01-31 12:40:59 +0500

    HomeWork1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int numTask1 = Convert.ToInt32(Console.ReadLine());
+int resultTask1 = -1;
+if (numTask1 % 100 == 10)
+{
+    resultTask1 = (numTask1 / 10) % 10;
+}
+Console.WriteLine($"Hello, World! {resultTask1}");
